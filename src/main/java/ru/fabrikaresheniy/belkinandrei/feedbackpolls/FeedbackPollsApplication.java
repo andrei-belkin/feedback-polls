59fb076d4003c12a689481d4ce6fd01302582c81
Белкин, Андрей <hh.mw1w0@slmail.me>
2022-04-07 17:36:37 -0400

    Точка пуска

---
+++ src/main/java/ru/fabrikaresheniy/belkinandrei/feedbackpolls/FeedbackPollsApplication.java
@@ -0,0 +1,13 @@
+package ru.fabrikaresheniy.belkinandrei.feedbackpolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackPollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackPollsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/fabrikaresheniy/belkinandrei/feedbackpolls/FeedbackPollsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fabrikaresheniy.belkinandrei.feedbackpolls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackPollsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
